e4974807aec0b23eef5e4630d48812a69b3b7ac7
Azaan-K <Azaan10141@gmail.com>
2024-07-24 19:20:35 -0600

    Added comments explaining the basic c++ code

--- Fundamentals/FundamentalsC++/FundamentalsC++.cpp
+++ Fundamentals/FundamentalsC++/FundamentalsC++.cpp
@@ -1,11 +1,18 @@
 // FundamentalsC++.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
+#include <iostream> 
+// Pre-processor directive that instructs the compiler to locate the file that contains code for the library known as 'iostream'.
 
+// This is defines the main function with a return value of int. 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Hello World!\n" << std::endl;
+    // std::cout is used for output.
+    // The << operator is used to send the string "Hello World!" to std::cout.
+    // std::endl is used to insert a newline character into the output stream.
+
+    return 0; // returns the int value of 0, indidcating the successful execution of the program.
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
